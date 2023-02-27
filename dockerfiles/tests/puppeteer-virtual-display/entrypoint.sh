bba6f16c46cd332f3a692a8c25fce55c17971f57
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-02-27 11:14:55 +0900

    feat: shared

--- dockerfiles/tests/puppeteer-virtual-display/entrypoint.sh
+++ dockerfiles/tests/puppeteer-virtual-display/entrypoint.sh
@@ -2,7 +2,7 @@
 rm /tmp/.X99-lock || true
 
 Xvfb :99 -ac -screen 0 600x800x16 -listen tcp &
-x11vnc -forever -noxdamage -display :99 -nopw -loop -xkb &
+x11vnc -shared -forever -noxdamage -display :99 -nopw -loop -xkb &
 
 node index.js
 
